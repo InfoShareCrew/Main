9eecc3677413e432fe521a19e2d9dadf97a94044
jinainkorea <kimjina1152@gmail.com>
2024-10-02 12:33:30 +0900

    init

---
+++ src/main/java/com/infoShare/calog/global/jpa/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.infoShare.calog.global.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@SuperBuilder
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
---
+++ src/test/java/com/infoShare/calog/CalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoShare.calog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
