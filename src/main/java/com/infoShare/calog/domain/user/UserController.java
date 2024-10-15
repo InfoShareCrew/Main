530124f871ff69e1a24ff52efc8f4585db0ef2d2
anyeok <https://github.com/anyeok>
2024-10-15 13:04:09 +0900

    kakao login complete

--- src/main/java/com/infoShare/calog/domain/user/UserController.java
+++ src/main/java/com/infoShare/calog/domain/user/UserController.java
@@ -1,6 +1,5 @@
 package com.infoShare.calog.domain.user;
 
-import com.infoShare.calog.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -9,17 +8,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
-
 
     @GetMapping("/signup")
     public String signup(UserCreateForm userCreateForm) {
@@ -38,11 +33,9 @@ public class UserController {
         try {
             userService.join(userCreateForm.getEmail(), userCreateForm.getNickname(), userCreateForm.getPassword1());
         } catch (DataIntegrityViolationException e) {
-            e.printStackTrace();
             bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
             return "signup_form";
         } catch (Exception e) {
-            e.printStackTrace();
             bindingResult.reject("signupFailed", e.getMessage());
             return "signup_form";
         }
@@ -52,30 +45,6 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
         return "login_form";
-    }
-
-    @GetMapping("/login/oauth2/callback/kakao")
-    public String kakaoCallback(@AuthenticationPrincipal SiteUser siteUser, Model model) {
-        String email = siteUser.getEmail();
-        String nickname = siteUser.getNickname();
-
-        // UserService를 통해 사용자 정보 처리
-        userService.processKakaoUser(email, nickname);
-
-        model.addAttribute("userName", nickname);
-        return "welcome"; // 사용자 환영 페이지
-    }
-
-    @GetMapping("/login/oauth2/callback/google")
-    public String googleCallback(@AuthenticationPrincipal SiteUser siteUser, Model model) {
-        String email = siteUser.getEmail();
-        String nickname = siteUser.getNickname();
-
-        // UserService를 통해 사용자 정보 처리
-        userService.processGoogleUser(email, nickname);
-
-        model.addAttribute("userName", nickname);
-        return "welcome"; // 사용자 환영 페이지
     }
 
     @PostMapping("/assign-role")
