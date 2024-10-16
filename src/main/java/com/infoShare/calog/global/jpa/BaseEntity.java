e340317750631b926f37794a88acfcbfbfecb414
anyeok <https://github.com/anyeok>
2024-10-16 22:02:16 +0900

    login error catch complete

--- src/main/java/com/infoShare/calog/global/jpa/BaseEntity.java
+++ src/main/java/com/infoShare/calog/global/jpa/BaseEntity.java
@@ -24,33 +24,4 @@ public class BaseEntity {
     private LocalDateTime createdDate = LocalDateTime.now();
     @LastModifiedDate
     private LocalDateTime modifiedDate;
-
-//    private BaseEntity(Builder builder) {
-//        this.id  = builder().id;
-//    }
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public LocalDateTime getCreatedDate() {
-//        return createdDate;
-//    }
-//
-//    public LocalDateTime getModifiedDate() {
-//        return modifiedDate;
-//    }
-//    public static class Builder {
-//        private Long id;
-//        private LocalDateTime createdDate;
-//        private LocalDateTime modifiedDate;
-//
-//        public Builder modifiedDate(LocalDateTime modifiedDate) {
-//            this.modifiedDate = modifiedDate; // 수정
-//            return this;
-//        }
-//
-//        public BaseEntity build() {
-//            return new BaseEntity(this);
-//        }
-//    }
 }
